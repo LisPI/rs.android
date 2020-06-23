cf03cdad6f930909d31a5c0c16ffc0cd27ed477a
LisPI <paschka.lis@gmail.com>
2020-06-23 23:24:02 +0300

    feat: implement simple menu

--- WorkingWithStorage/app/src/main/java/com/develop/rs_school/workingwithstorage/MainActivity.kt
+++ WorkingWithStorage/app/src/main/java/com/develop/rs_school/workingwithstorage/MainActivity.kt
@@ -3,6 +3,7 @@ package com.develop.rs_school.workingwithstorage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
 import com.develop.rs_school.workingwithstorage.databinding.ActivityMainBinding
 import com.develop.rs_school.workingwithstorage.settings.SettingsActivity
 
@@ -20,4 +21,23 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settingsActivityMenuItem -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
