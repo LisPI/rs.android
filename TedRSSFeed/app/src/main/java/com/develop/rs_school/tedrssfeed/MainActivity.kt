42699b3b722a165d23c26276e8501414f8be9419
LisPI <paschka.lis@gmail.com>
2020-07-27 12:36:13 +0300

    ref: apply ktLint, detekt

--- TedRSSFeed/app/src/main/java/com/develop/rs_school/tedrssfeed/MainActivity.kt
+++ TedRSSFeed/app/src/main/java/com/develop/rs_school/tedrssfeed/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.develop.rs_school.tedrssfeed
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
