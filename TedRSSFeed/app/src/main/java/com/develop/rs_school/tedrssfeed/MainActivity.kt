d97374cf1794a6bd0832819eb236ea8a59509d92
LisPI <paschka.lis@gmail.com>
2020-07-29 00:10:08 +0300

    feat: add data to assets, read for test

--- TedRSSFeed/app/src/main/java/com/develop/rs_school/tedrssfeed/MainActivity.kt
+++ TedRSSFeed/app/src/main/java/com/develop/rs_school/tedrssfeed/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.develop.rs_school.tedrssfeed
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO add parser. It is just for fun
+        val jsonString = this.assets.open("data.json").bufferedReader().use { it.readText() }
+        val json = JSONObject(jsonString)
+        val itemsArray = json.getJSONObject("channel").getJSONArray("item")
+        findViewById<TextView>(R.id.text).text = itemsArray.getJSONObject(0).getString("title")
     }
 }
